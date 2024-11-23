d9447b29e7f8002547047735882ec4ed11057d54
Arturo Burigo <burigoarturo3@gmail.com>
2024-11-23 00:14:42 -0300

    final

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/', // Garante que o base path esteja configurado corretamente
+  build: {
+    outDir: 'dist', // Certifique-se de que os arquivos gerados sejam exportados corretamente
+  },
+  server: {
+    port: 3000, // Porta opcional para ambiente local
+    open: true, // Abrir automaticamente no navegador
+  },
+  preview: {
+    port: 5000, // Porta do comando "vite preview"
+  },
+});
